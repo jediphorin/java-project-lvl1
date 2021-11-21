7ca5b8b779c429f0f0e6b3a06f6df19e323ed3ce
Rinat Kamaletdinov <garrett86@yandex.ru>
2021-11-21 18:31:08 +0500

    changed App.java and Cli.java; added games/Even.java

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Cli {
 
+    public static String name;
+//    private String name;
+//    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
     public static void nameSetting() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name);
+        String localName = scanner.nextLine();
+        System.out.println("Hello, " + localName);
+        Cli.name = localName;
     }
 }
---
+++ src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,38 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Even {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void evenNumber() {
+        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+        scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            Random random = new Random();
+            int number = random.nextInt();
+            System.out.println("Question: " + number);
+            System.out.print("Your answer: ");
+            String yourAnswer = scanner.nextLine();
+            String trueAnswer;
+            if (number % 2 == 0) {
+                trueAnswer = "yes";
+            } else {
+                trueAnswer = "no";
+            }
+
+            if (yourAnswer.equals(trueAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                i = -1;
+                System.out.println("'" + yourAnswer + "' is wrong answer ;(. Correct answer was '" + trueAnswer + "'");
+            }
+        }
+        System.out.println("Congratulations, " + Cli.name +"!");
+    }
+}
