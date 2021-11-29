1af636f9d27fd42e6a8b403de9d5d314ce2f1adb
Rinat Kamaletdinov <garrett86@yandex.ru>
2021-11-30 03:02:22 +0500

    вся логика запуска игр перенесена в Engine.java; в Engine.java выбор игр переделан в switch-case вид

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -2,15 +2,53 @@ package hexlet.code;
 
 import hexlet.code.games.*;
 
-public class Engine {
-    private int gameNumber;
+import java.util.Scanner;
 
-    public Engine(int gameNumber) {
-        this.gameNumber = gameNumber;
+public class Engine {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+//    public Engine(int gameNumber) {
+//        this.gameNumber = gameNumber;
+//    }
+
+    public Engine() {
     }
 
     public void executeTheGame() {
-        if (gameNumber != 0 ) {
+
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GDC\n5 - Progression\n6 - Prime\n0 - Exit");
+        System.out.print("Your choice: ");
+        int gameNumber = scanner.nextInt();
+
+        if (gameNumber != 0) {
+            System.out.println("\nWelcome to the Brain Games!");
+            Cli.nameSetting();
+
+            switch (gameNumber) {
+                case 2:
+                    Even.evenNumber();
+                    break;
+                case 3:
+                    Calc calc = new Calc();
+                    calc.calculatorTheGame();
+                    break;
+                case 4:
+                    GDC gdc = new GDC();
+                    gdc.GDCTheGame();
+                    break;
+                case 5:
+                    Progression progression = new Progression();
+                    break;
+                case 6:
+                    Prime prime = new Prime();
+                    prime.primeTheGame();
+                    break;
+                default:
+            }
+        }
+
+        /*if (gameNumber != 0 ) {
             System.out.println("\nWelcome to the Brain Games!");
             Cli.nameSetting();
             if (gameNumber == 2) {
@@ -27,6 +65,6 @@ public class Engine {
                 Prime prime = new Prime();
                 prime.primeTheGame();
             }
-        }
+        }*/
     }
 }
