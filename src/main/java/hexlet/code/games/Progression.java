c84bdda1924fde6b087683c33662749adffc8939
Rinat Kamaletdinov <garrett86@yandex.ru>
2021-12-11 00:34:22 +0500

    порубил на части метод игры в прогрессию

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -7,53 +7,71 @@ public class Progression {
     public static void theProgression() {
         final String rulesProgression = "What number is missing in the progression?";
         String[][] result = Engine.createTheArrayForQuestionsAndTrueAnswers();
-
         for (int i = 0; i < Engine.CYCLES; i++) {
             //  массив прогрессирующих значений рандомной длины
-            final int upperLengthLimit = 10;
-            final int lowerLengthLimit = 5;
-            int lengthProgression = Engine.randomNumber(upperLengthLimit, lowerLengthLimit);
-            int[] progression = new int[lengthProgression];
+            int[] progression = initializeTheProgression();
 
             //  первое значение в прогрессии
-            final int upperStartPositionRandomLimit = 14;
-            final int lowerStartPositionRandomLimit = 2;
-            int startProgression = Engine.randomNumber(upperStartPositionRandomLimit, lowerStartPositionRandomLimit);
+            int startProgression = startValueProgression();
 
             //  прогрессия прогрессии
-            final int upperProgressionShiftRandomLimit = 6;
-            final int lowerProgressionShiftRandomLimit = 2;
-            int shift = Engine.randomNumber(upperProgressionShiftRandomLimit, lowerProgressionShiftRandomLimit);
+            int shift = shiftValueProgression();
 
             //  элемент для вопроса
-            final int elementsInProgression = progression.length - 1;
-            int indexForQuestion = Engine.randomNumber(elementsInProgression);
+            int indexForQuestion = indexForQuestionSetting(progression);
 
             //  заполнение прогрессии
             for (int j = 0, k = startProgression; j < progression.length; j++, k += shift) {
                 progression[j] = k;
             }
-
             //  заполнение ответов
             result[i][1] = String.valueOf(progression[indexForQuestion]);
 
             //  заполнение вопросов
-            for (int j = 0; j < progression.length; j++) {
-                if (indexForQuestion != j) {
-                    if (j != 0) {
-                        result[i][0] += (" " + progression[j]);
-                    } else {
-                        result[i][0] = String.valueOf(progression[j]);
-                    }
+            fillTheQuestions(progression, indexForQuestion, result, i);
+        }
+        Engine.theGame(result, rulesProgression);
+    }
+
+    private static int[] initializeTheProgression() {
+        final int upperLengthLimit = 10;
+        final int lowerLengthLimit = 5;
+        int lengthProgression = Engine.randomNumber(upperLengthLimit, lowerLengthLimit);
+        return new int[lengthProgression];
+    }
+
+    private static int startValueProgression() {
+        final int upperStartPositionRandomLimit = 14;
+        final int lowerStartPositionRandomLimit = 2;
+        return Engine.randomNumber(upperStartPositionRandomLimit, lowerStartPositionRandomLimit);
+    }
+
+    private static int shiftValueProgression() {
+        final int upperProgressionShiftRandomLimit = 6;
+        final int lowerProgressionShiftRandomLimit = 2;
+        return Engine.randomNumber(upperProgressionShiftRandomLimit, lowerProgressionShiftRandomLimit);
+    }
+
+    private static int indexForQuestionSetting(int[] progression) {
+        final int elementsInProgression = progression.length - 1;
+        return Engine.randomNumber(elementsInProgression);
+    }
+
+    private static void fillTheQuestions(int[] progression, int indexForQuestion, String[][] result, int i) {
+        for (int j = 0; j < progression.length; j++) {
+            if (indexForQuestion != j) {
+                if (j != 0) {
+                    result[i][0] += (" " + progression[j]);
                 } else {
-                    if (j != 0) {
-                        result[i][0] += " ..";
-                    } else {
-                        result[i][0] = "..";
-                    }
+                    result[i][0] = String.valueOf(progression[j]);
+                }
+            } else {
+                if (j != 0) {
+                    result[i][0] += " ..";
+                } else {
+                    result[i][0] = "..";
                 }
             }
         }
-        Engine.theGame(result, rulesProgression);
     }
 }
